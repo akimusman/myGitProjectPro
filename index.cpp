970752e9967ccc366201989878971dbbae876775
robel122 <60172989+robel122@users.noreply.github.com>
2021-06-15 20:15:53 +0300

    This is my word for aastu

--- index.cpp
+++ index.cpp
@@ -3,5 +3,6 @@ using namespace std;
 int main(){
 cout<<"hi . i am akem usman";
   cout<<" lets talk about AASTU LIFE";
+  cout<<" i am robel. AASTU IS wanderful place to make difference ";
 return 0;
 }
